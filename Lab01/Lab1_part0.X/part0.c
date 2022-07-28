3e6a716f2fdd1a04ca0653b7571f0fdbe2eab229
Bijaya Khadka <kbj500@gmail.com>
2022-07-27 23:04:01 -0700

    created Main file

---
+++ Lab01/Lab1_part0.X/part0.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   part0.c
+ * Author: bijayakhadka
+ *
+ * Created on July 27, 2022, 11:02 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
