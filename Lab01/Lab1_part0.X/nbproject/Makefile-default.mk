266372388ff115ffab8cd5a70f433023de0d2990
Bijaya Khadka <kbj500@gmail.com>
2022-07-29 15:25:16 -0700

    Part 0.1 finished

--- Lab01/Lab1_part0.X/nbproject/Makefile-default.mk
+++ Lab01/Lab1_part0.X/nbproject/Makefile-default.mk
@@ -90,7 +90,7 @@ ifneq ($(INFORMATION_MESSAGE), )
 endif
 	${MAKE}  -f nbproject/Makefile-default.mk ${DISTDIR}/Lab1_part0.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
 
-MP_PROCESSOR_OPTION=32MX340F512H
+MP_PROCESSOR_OPTION=32MX320F128H
 MP_LINKER_FILE_OPTION=
 # ------------------------------------------------------------------------------------
 # Rules for buildStep: assemble
