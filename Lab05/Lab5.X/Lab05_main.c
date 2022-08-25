ed15285b12eafc752e15ec5ff230965e3177868d
Bijaya Khadka <kbj500@gmail.com>
2022-08-25 00:40:38 -0700

    worked up to countdown

--- Lab05/Lab5.X/Lab05_main.c
+++ Lab05/Lab5.X/Lab05_main.c
@@ -1,9 +1,13 @@
 // **** Include libraries here ****
 // Standard libraries
 #include <stdio.h>
+#include <string.h>
 
 //CSE13E Support Library
 #include "BOARD.h"
+#include "Oled.h"
+#include "Buttons.h"
+#include "Adc.h"
 
 // Microchip libraries
 #include <xc.h>
@@ -15,6 +19,21 @@
 // Set a macro for resetting the timer, makes the code a little clearer.
 #define TIMER_2HZ_RESET() (TMR1 = 0)
 
+#define LONG_PRESS 3
+
+#define START_TEMP 300
+
+#define BYTE_TO_BINARY_PATTERN "%c%c%c%c%c%c%c%c" /// macros for printing binary numbers
+#define BYTE_TO_BINARY(byte)  \
+  (byte & 0x80 ? '1' : '0'), \
+  (byte & 0x40 ? '1' : '0'), \
+  (byte & 0x20 ? '1' : '0'), \
+  (byte & 0x10 ? '1' : '0'), \
+  (byte & 0x08 ? '1' : '0'), \
+  (byte & 0x04 ? '1' : '0'), \
+  (byte & 0x02 ? '1' : '0'), \
+  (byte & 0x01 ? '1' : '0') 
+
 
 // **** Set any local typedefs here ****
 typedef enum {
@@ -22,14 +41,42 @@ typedef enum {
 } OvenState;
 
 typedef struct {
-    OvenState state;
+    OvenState state; //Oven State
+    int temperature; //Oven temperature
+    char cm[5]; //string holding cooking mode
+    int bpt; //Button Press Time
+    int cst; //Cooking Start Time
+    int inS; //Input Selector
     //add more members to this struct
 } OvenData;
 
 // **** Declare any datatypes here ****
-
+enum MODE{
+    bake, toast, broil,
+};
 // **** Define any module-level, global, or external variables here ****
+static uint8_t ButtonResult;
+static OvenData od;
+static int sp = 12;
+static uint8_t adcC;
+static uint32_t adcV;
+static int TIMER_TICK;
 
+static int st; // start time
+static int cft; //current free running time
+static int et; //elapsed time
+static int m; //mode setting
+static int min;//minute
+static int seconds;//seconds
+static int temp = START_TEMP; //temperature
+
+static char mo[5] = "Mode:"; //char to hold mode string for display
+static char ti[5] = "Time:"; // char to hold time string for display
+static char ati[4]; // char to hold minute/seconds input for display
+static char atp[4]; // char to hold temp input for display
+static char tp[5] = "Temp:"; // char to hold temp string for display
+
+static const char *MODE_LIST[] = {"Bake","Toast","Broil"} ;
 
 // **** Put any helper functions here ****
 
@@ -37,6 +84,79 @@ typedef struct {
 /*This function will update your OLED to reflect the state .*/
 void updateOvenOLED(OvenData ovenData){
     //update OLED here
+    // create starting screen
+          //heading
+          OledClear(0);
+          
+          OledDrawChar(62,1,mo[0]);     //write out to OLED with each character in mode
+          OledDrawChar(68,1,mo[1]);
+          OledDrawChar(74,1,mo[2]);
+          OledDrawChar(80,1,mo[3]);
+          OledDrawChar(86,1,mo[4]);
+          OledDrawChar(92,1,mo[5]);
+          
+          OledDrawChar(96,1,od.cm[0]);     //write out to OLED with each character in Oven Data mode
+          OledDrawChar(102,1,od.cm[1]);
+          OledDrawChar(108,1,od.cm[2]);
+          OledDrawChar(114,1,od.cm[3]);
+          OledDrawChar(120,1,od.cm[4]);
+          OledDrawChar(126,1,od.cm[5]);
+          
+          
+          
+          
+          //percent = ((float) raw)/1023 * 100; //percent of raw voltage
+          //sprintf(p, "%3.0f", percent); //set percent value to char string
+          
+          OledDrawChar(62,12,ti[0]);    // write out to OLED with each character in time
+          OledDrawChar(68,12,ti[1]);
+          OledDrawChar(74,12,ti[2]);
+          OledDrawChar(80,12,ti[3]);
+          OledDrawChar(86,12,ti[4]);
+          OledDrawChar(92,12,ti[5]);
+          
+          
+                                        // write out to OLED dynamic cooking time char
+          
+          sprintf(ati, "%d", min); //set minute value to char string
+          sprintf(ati+strlen(ati),"%d",seconds); //set seconds value to char string
+          printf("ati:%s",ati);
+          if(seconds < 10){
+          OledDrawChar(100,12,ati[0]);
+          OledDrawChar(106,12,':');
+          OledDrawChar(112,12,'0');
+          OledDrawChar(118,12,ati[1]);
+          }
+          else{
+          OledDrawChar(100,12,ati[0]);
+          OledDrawChar(106,12,':');
+          OledDrawChar(112,12,ati[1]);
+          OledDrawChar(118,12,ati[2]);
+          }
+          
+          if(strcmp(od.cm, MODE_LIST[1]) != 0){
+          OledDrawChar(62,24,tp[0]);    // write out to OLED with each character in temperature
+          OledDrawChar(68,24,tp[1]);
+          OledDrawChar(74,24,tp[2]);
+          OledDrawChar(80,24,tp[3]);
+          OledDrawChar(86,24,tp[4]);
+          
+          sprintf(atp, "%d", temp); //set temperature value to char string
+          //printf("atp:%s",atp);
+          OledDrawChar(100,24,atp[0]);
+          OledDrawChar(106,24,atp[1]);
+          OledDrawChar(112,24,atp[2]);
+          
+          }
+          
+          //printf("sp(draw):%d\n",sp);
+          OledDrawChar(54,sp,'>');
+          OledUpdate();                 //update OLED
+          
+          
+          
+      
+    
 }
 
 /*This function will execute your state machine.  
@@ -44,13 +164,135 @@ void updateOvenOLED(OvenData ovenData){
 void runOvenSM(void)
 {
     //write your SM logic here.
+    //check timer ticks
+    if(od.state == RESET_PENDING){
+        od.state = SETUP;
+        
+        adcV = (((uint32_t)AdcRead())>>2)+1;
+        min = adcV/60;
+        seconds = adcV % 60;
+    }
+    if(sp == 12){
+        if(adcC){
+           
+            
+            //printf("\nAdcRead() int:%d\n",AdcRead());
+            //printf("AdcRead() binary:\n "BYTE_TO_BINARY_PATTERN, BYTE_TO_BINARY(AdcRead()));
+            adcV = (((uint32_t)AdcRead())>>2)+1;
+            min = adcV/60;
+            printf("Minutes:%d\n",min);
+            seconds = adcV % 60;
+            
+            printf("Seconds:%d\n",seconds);
+            //printf("\nadcV:%d\n",adcV);
+            //printf("adcV binary:\n "BYTE_TO_BINARY_PATTERN, BYTE_TO_BINARY(adcV));
+            
+        }
+    }
+    else{
+        if(adcC){
+        adcV = (((uint32_t)AdcRead())>>2)+300;
+        printf("\nadcV:%d\n",adcV);
+        temp = adcV;
+        }
+        
+    }
+    if(TIMER_TICK){
+        cft++;
+        et = cft-st;
+        TIMER_TICK = 0;
+        if(od.state == COOKING){
+            printf("Seconds(cook):%d\n",seconds);
+            if(seconds != 0 || min != 0){
+                if(seconds == 0){
+                    min--;
+                    seconds = 59;
+                }
+                seconds--;
+            }
+            else{
+                od.state = RESET_PENDING;
+            }
+            updateOvenOLED(od);
+            /*
+            if(seconds != 0 || min != 0){
+                if(seconds = 0){
+                    printf("Minutes(cs):%d\n",min);
+                    min--;
+                    seconds = 59;
+                }
+            printf("Minutes(c):%d\n",min);
+                
+            printf("Seconds(c1b):%d\n",seconds);
+               seconds--; 
+            printf("Seconds(c1a):%d\n",seconds);
+               updateOvenOLED(od);
+               
+            }
+            else{
+                od.state == RESET_PENDING; 
+            }*/
+            printf("Seconds(c):%d\n",seconds);
+        }
+    }
+    if(ButtonResult & BUTTON_EVENT_3DOWN) {
+        st = cft;
+        et = cft-st;
+        od.state = SELECTOR_CHANGE_PENDING;
+        printf("\nst:%d\n", st);
+        
+       printf("\ncft(down):%d\n", cft);
+    }
+    if(ButtonResult & BUTTON_EVENT_3UP){ 
+        
+            printf("\ncft(Up):%d\n", cft);
+             printf("\net:%d\n", et);
+                 if(et < LONG_PRESS){
+                 //printf("\nswitch next mode\n");
+                    sp = 12;
+                    if(m < 2){
+                        m++;
+                        strcpy(od.cm,MODE_LIST[m]);
+                        updateOvenOLED(od);
+                    }
+                    else{
+                        m = 0;
+                        strcpy(od.cm,MODE_LIST[m]);
+                        updateOvenOLED(od);
+                    }
+                 }
+                 else{
+                     if(strcmp(od.cm, MODE_LIST[1]) != 0){
+                     if(sp>12){
+                           sp = 12;
+                           updateOvenOLED(od);
+                           //printf("sp(12):%d\n",sp);
+
+                       }
+                       else{
+                           sp = 24;
+                           updateOvenOLED(od);
+                           //printf("sp(24):%d\n",sp);
+
+                       }
+                     }
+
+                 }
+             od.state = SETUP;
+    }
+    
+    if(ButtonResult & BUTTON_EVENT_4DOWN){
+        od.state = COOKING; 
+    }
+    
+    updateOvenOLED(od);
+    
 }
 
 
 int main()
 {
     BOARD_Init();
-
      //initalize timers and timer ISRs:
     // <editor-fold defaultstate="collapsed" desc="TIMER SETUP">
     
@@ -87,12 +329,28 @@ int main()
     printf("Welcome to CRUZID's Lab05 (Toaster Oven).  Compiled on %s %s.", __TIME__, __DATE__);
 
     //initialize state machine (and anything else you need to init) here
-
+    OledInit();
+    AdcInit();
+    od.state = SETUP;
+    strcpy(od.cm,MODE_LIST[m]);
     while (1){
         // Add main loop code here:
-        // check for events
-        // on event, run runOvenSM()
-        // clear event flags
+        
+                        //check for events
+    if(ButtonResult || adcC){   //check for events: button, adc, state
+        runOvenSM();                                // on event, run runOvenSM()
+    }   
+    if(od.state == SELECTOR_CHANGE_PENDING){// if state is selector change pending 
+        runOvenSM(); 
+    }
+    if(od.state == SETUP || od.state == COOKING || od.state == RESET_PENDING){ // if state is setup, cooking, reset pending
+        runOvenSM(); 
+    }
+    //ButtonResult = 0;   // clear event flags
+    //adcC = 0;
+    
+        
+        
     };
 }
 
@@ -104,6 +362,10 @@ void __ISR(_TIMER_3_VECTOR, ipl4auto) TimerInterrupt5Hz(void)
     IFS0CLR = 1 << 12;
 
     //add event-checking code here
+    TIMER_TICK = 1;
+    
+    
+    
 }
 
 
@@ -114,4 +376,7 @@ void __ISR(_TIMER_2_VECTOR, ipl4auto) TimerInterrupt100Hz(void)
     IFS0CLR = 1 << 8;
 
     //add event-checking code here
+    
+    ButtonResult = ButtonsCheckEvents(); //collect new button activity
+    adcC = AdcChanged(); //collect adc activity 
 }
--- Lab05/Lab5.X/nbproject/Makefile-default.mk
+++ Lab05/Lab5.X/nbproject/Makefile-default.mk
@@ -107,17 +107,17 @@ endif
 # ------------------------------------------------------------------------------------
 # Rules for buildStep: compile
 ifeq ($(TYPE_IMAGE), DEBUG_RUN)
-${OBJECTDIR}/_ext/812474809/BOARD.o: /Users/bijayakhadka/Documents/ECE_13/bkhadka/Lab05/Lab5.X/BOARD.c  .generated_files/flags/default/8498479764adfa4fed7f5f98d5f65dd04fcfb7f9 .generated_files/flags/default/b049a9946035c0d6f8c1361b9c30a33141c1b72d
+${OBJECTDIR}/_ext/812474809/BOARD.o: /Users/bijayakhadka/Documents/ECE_13/bkhadka/Lab05/Lab5.X/BOARD.c  .generated_files/flags/default/df41249c67920c9b2d4c821520cea1a0bbd96258 .generated_files/flags/default/b049a9946035c0d6f8c1361b9c30a33141c1b72d
 	@${MKDIR} "${OBJECTDIR}/_ext/812474809" 
 	@${RM} ${OBJECTDIR}/_ext/812474809/BOARD.o.d 
 	@${RM} ${OBJECTDIR}/_ext/812474809/BOARD.o 
-	${MP_CC}  $(MP_EXTRA_CC_PRE) -g -D__DEBUG   -fframe-base-loclist  -x c -c -mprocessor=$(MP_PROCESSOR_OPTION)  -fno-common -MP -MMD -MF "${OBJECTDIR}/_ext/812474809/BOARD.o.d" -o ${OBJECTDIR}/_ext/812474809/BOARD.o /Users/bijayakhadka/Documents/ECE_13/bkhadka/Lab05/Lab5.X/BOARD.c    -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)  -mdfp="${DFP_DIR}"  
+	${MP_CC}  $(MP_EXTRA_CC_PRE) -g -D__DEBUG -D__MPLAB_DEBUGGER_PK3=1  -fframe-base-loclist  -x c -c -mprocessor=$(MP_PROCESSOR_OPTION)  -fno-common -MP -MMD -MF "${OBJECTDIR}/_ext/812474809/BOARD.o.d" -o ${OBJECTDIR}/_ext/812474809/BOARD.o /Users/bijayakhadka/Documents/ECE_13/bkhadka/Lab05/Lab5.X/BOARD.c    -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)  -mdfp="${DFP_DIR}"  
 	
-${OBJECTDIR}/_ext/812474809/Lab05_main.o: /Users/bijayakhadka/Documents/ECE_13/bkhadka/Lab05/Lab5.X/Lab05_main.c  .generated_files/flags/default/c6ce45fae32c68e5bc77e8bfc0d82f08d0f34963 .generated_files/flags/default/b049a9946035c0d6f8c1361b9c30a33141c1b72d
+${OBJECTDIR}/_ext/812474809/Lab05_main.o: /Users/bijayakhadka/Documents/ECE_13/bkhadka/Lab05/Lab5.X/Lab05_main.c  .generated_files/flags/default/ceca9399fb8296e6badc09b5e594369ba3aeeb69 .generated_files/flags/default/b049a9946035c0d6f8c1361b9c30a33141c1b72d
 	@${MKDIR} "${OBJECTDIR}/_ext/812474809" 
 	@${RM} ${OBJECTDIR}/_ext/812474809/Lab05_main.o.d 
 	@${RM} ${OBJECTDIR}/_ext/812474809/Lab05_main.o 
-	${MP_CC}  $(MP_EXTRA_CC_PRE) -g -D__DEBUG   -fframe-base-loclist  -x c -c -mprocessor=$(MP_PROCESSOR_OPTION)  -fno-common -MP -MMD -MF "${OBJECTDIR}/_ext/812474809/Lab05_main.o.d" -o ${OBJECTDIR}/_ext/812474809/Lab05_main.o /Users/bijayakhadka/Documents/ECE_13/bkhadka/Lab05/Lab5.X/Lab05_main.c    -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)  -mdfp="${DFP_DIR}"  
+	${MP_CC}  $(MP_EXTRA_CC_PRE) -g -D__DEBUG -D__MPLAB_DEBUGGER_PK3=1  -fframe-base-loclist  -x c -c -mprocessor=$(MP_PROCESSOR_OPTION)  -fno-common -MP -MMD -MF "${OBJECTDIR}/_ext/812474809/Lab05_main.o.d" -o ${OBJECTDIR}/_ext/812474809/Lab05_main.o /Users/bijayakhadka/Documents/ECE_13/bkhadka/Lab05/Lab5.X/Lab05_main.c    -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)  -mdfp="${DFP_DIR}"  
 	
 else
 ${OBJECTDIR}/_ext/812474809/BOARD.o: /Users/bijayakhadka/Documents/ECE_13/bkhadka/Lab05/Lab5.X/BOARD.c  .generated_files/flags/default/d2b8192c53ccb9e4cbb04cc4c238a33005b18e0c .generated_files/flags/default/b049a9946035c0d6f8c1361b9c30a33141c1b72d
@@ -145,7 +145,7 @@ endif
 ifeq ($(TYPE_IMAGE), DEBUG_RUN)
 ${DISTDIR}/Lab5.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}: ${OBJECTFILES}  nbproject/Makefile-${CND_CONF}.mk  /Users/bijayakhadka/Documents/ECE_13/bkhadka/Lab05/Lab5.X/Lab5SupportLib.a  
 	@${MKDIR} ${DISTDIR} 
-	${MP_CC} $(MP_EXTRA_LD_PRE) -g   -mprocessor=$(MP_PROCESSOR_OPTION)  -o ${DISTDIR}/Lab5.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX} ${OBJECTFILES_QUOTED_IF_SPACED}    /Users/bijayakhadka/Documents/ECE_13/bkhadka/Lab05/Lab5.X/Lab5SupportLib.a      -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)      -Wl,--defsym=__MPLAB_BUILD=1$(MP_EXTRA_LD_POST)$(MP_LINKER_FILE_OPTION),--defsym=__MPLAB_DEBUG=1,--defsym=__DEBUG=1,-D=__DEBUG_D,--defsym=_min_heap_size=1024,--no-code-in-dinit,--no-dinit-in-serial-mem,-Map="${DISTDIR}/${PROJECTNAME}.${IMAGE_TYPE}.map",--memorysummary,${DISTDIR}/memoryfile.xml -mdfp="${DFP_DIR}"
+	${MP_CC} $(MP_EXTRA_LD_PRE) -g -mdebugger -D__MPLAB_DEBUGGER_PK3=1 -mprocessor=$(MP_PROCESSOR_OPTION)  -o ${DISTDIR}/Lab5.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX} ${OBJECTFILES_QUOTED_IF_SPACED}    /Users/bijayakhadka/Documents/ECE_13/bkhadka/Lab05/Lab5.X/Lab5SupportLib.a      -DXPRJ_default=$(CND_CONF)    $(COMPARISON_BUILD)   -mreserve=data@0x0:0x1FC -mreserve=boot@0x1FC02000:0x1FC02FEF -mreserve=boot@0x1FC02000:0x1FC024FF  -Wl,--defsym=__MPLAB_BUILD=1$(MP_EXTRA_LD_POST)$(MP_LINKER_FILE_OPTION),--defsym=__MPLAB_DEBUG=1,--defsym=__DEBUG=1,-D=__DEBUG_D,--defsym=__MPLAB_DEBUGGER_PK3=1,--defsym=_min_heap_size=1024,--no-code-in-dinit,--no-dinit-in-serial-mem,-Map="${DISTDIR}/${PROJECTNAME}.${IMAGE_TYPE}.map",--memorysummary,${DISTDIR}/memoryfile.xml -mdfp="${DFP_DIR}"
 	
 else
 ${DISTDIR}/Lab5.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}: ${OBJECTFILES}  nbproject/Makefile-${CND_CONF}.mk  /Users/bijayakhadka/Documents/ECE_13/bkhadka/Lab05/Lab5.X/Lab5SupportLib.a 
